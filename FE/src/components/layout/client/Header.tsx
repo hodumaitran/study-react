4583579c07b09feb26a833bcc5688cb6afeabd9a
vokhanhduy <vokhanhduy2004@gmail.com>
2025-07-22 10:13:11 +0700

    Update code

--- FE/src/components/layout/client/Header.tsx
+++ FE/src/components/layout/client/Header.tsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className="w-full flex justify-center items-center py-3 border-b border-gray-200">
       <div className="w-full container-wrapper flex justify-between items-center">
-        <div className="font-bold text-xl text-primary">Study React</div>
+        <div className="font-bold text-xl text-primary">
+          <Link to={"/"}>Study React</Link>
+        </div>
         <div className="flex justify-between items-center gap-8">
           <ul className="flex justify-center items-center gap-5">
             <li className="font-semibold hover:text-gray-500">
